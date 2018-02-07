87e3a71467b0f9eecd962e47027d5b49260a9fb9
Hannes <k.hannes.simonsson@gmail.com>
2018-02-07 22:49:37 +0100

    added options

--- someCode.hs
+++ someCode.hs
@@ -1,2 +1,10 @@
 --testing git with hello world
-hello = print "hello world"
+
+hello = do
+        putStrLn "enter 1 for first message or 2 for second message."
+        x <- getLine 
+        test x
+
+test x | x == "1" = print "hello world"
+       | x == "2" = print "try 1"
+       | otherwise = print "wrong"
